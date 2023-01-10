58b2014d29bdb086b45be7174aa3cd60248593c6
leeseongdooo <tjdgns8966@naver.com>
2023-01-10 15:47:13 +0900

    Day0110(2)

--- src/Component/SearchData/CharacterEquipment.jsx
+++ src/Component/SearchData/CharacterEquipment.jsx
@@ -6,32 +6,34 @@ function CharacterEquipment() {
     
     const Key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDExODYifQ.dp5Rwt6qAxGWBF6L00JpgQ8FRk0LC2McvjnYrcIdaVmlW1lcMOhWfDEuQ3d8PBB_bUevh03dw6Shx3sc8_X_B_cUja3eONQ0MWPPa9ZRvHYBjaBn4RPl4pe_M5quBOaQVhTBhcxNYJoCxVQhHfwf_0K0rmAEDHYdSICEIpeD-Ve8WaEBm7JXa36RBP-vefRtcIZh1O35knWa4bXCjuT4rodTYx4WiE_bt4sCUGfaPfzriAe6P5OjlkGx1YEkk3nYGJCVX-cfdIA5qPAc7612BrjV_YuXx5Qh8XzsPL6m5N9v-h-_GAEW10OWSYvxJabPYV8KhPMKanaEpdrpS6i6jA";
     const NickName = useParams();
-    const [EquipmentData, setEquipmentData] = useState({});
-    
-    async function GetEquipment() {
+    const [loading, setLoading] = useState(false);
+    const [EquipmentData, setEquipmentData] = useState(null);
 
+    const GetEquipment = async () =>  {
         try {
+            setLoading(true);
             const response = await axios.get(`https://developer-lostark.game.onstove.com/armories/characters/${NickName.searchCharacter}/equipment`, {
                 headers: {Authorization: `bearer ${Key}`}
-            })
-            console.log(response.data)  
+            });
             setEquipmentData(response.data);
         } catch (error) {
             console.log(error)
         }
+        setLoading(false);
     }
+
+
 
     useEffect(() => {
         GetEquipment();
-        console.log(EquipmentData)
-    }, [NickName])
+    }, [NickName]);
     
-
-
+  
+    if (loading) return <div>로딩중..</div>;
 
     return (
         <div>
-    
+           
         </div>
     )
 }
