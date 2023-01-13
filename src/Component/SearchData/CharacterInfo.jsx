9e88969b2806367f69acba15e99d3f52b107f6b1
leeseonghoon <tjdgns8966@naver.com>
2023-01-13 15:01:48 +0900

    Day0113(3)

--- src/Component/SearchData/CharacterInfo.jsx
+++ src/Component/SearchData/CharacterInfo.jsx
@@ -2,22 +2,54 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "../../Css/CharacterInfo.scss";
+import {GiCrestedHelmet} from 'react-icons/gi'
 import CharacterEngravings from "./CharacterEngravings";
 
+
+function NewDesign({SearchCharacter}) {{
+    return (
+        // 큰 div
+        <div className="NewCharacterInfoBox">
+            <div className="InformationArea">
+                <div className="TopArea">
+                    <div className="SimpleInformation">
+                        <span>Lv. {SearchCharacter.CharacterLevel}</span>
+                        <span>{SearchCharacter.CharacterClassName}</span>
+                        <span>@ {SearchCharacter.ServerName}</span>
+                    </div>
+                    
+                    <h2>{SearchCharacter.CharacterName}</h2>
+                    <GiCrestedHelmet /> 
+                    <h2>{SearchCharacter.ItemAvgLevel}</h2>
+                </div>
+
+                <div className="BottomArea">
+                    <p>원정대  {SearchCharacter.ExpeditionLevel}</p>
+                    <p>칭호 {SearchCharacter.Title}</p>
+                    <p>길드 {SearchCharacter.GuildName}</p>
+                    <p>영지: Lv{SearchCharacter.TownLevel} {SearchCharacter.TownName}</p>
+                    <p>PVP {SearchCharacter.PvpGradeName}</p>
+                </div>
+            </div>
+            
+            <div>
+                {/* 사진 이미지 */}
+            </div>
+        </div>
+    )
+}}
 
 
 function CharacterInfo({SearchCharacter, CharacterStats}) {
 
     const Key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDExODYifQ.dp5Rwt6qAxGWBF6L00JpgQ8FRk0LC2McvjnYrcIdaVmlW1lcMOhWfDEuQ3d8PBB_bUevh03dw6Shx3sc8_X_B_cUja3eONQ0MWPPa9ZRvHYBjaBn4RPl4pe_M5quBOaQVhTBhcxNYJoCxVQhHfwf_0K0rmAEDHYdSICEIpeD-Ve8WaEBm7JXa36RBP-vefRtcIZh1O35knWa4bXCjuT4rodTYx4WiE_bt4sCUGfaPfzriAe6P5OjlkGx1YEkk3nYGJCVX-cfdIA5qPAc7612BrjV_YuXx5Qh8XzsPL6m5N9v-h-_GAEW10OWSYvxJabPYV8KhPMKanaEpdrpS6i6jA";
     const NickName = useParams();
-   
-
-
+    console.log(SearchCharacter);
     return (
         
         <div className="CharacterSearchResult">
-          
-          <div className="profileArea">
+            <NewDesign SearchCharacter={SearchCharacter}/>
+          {/* <div className="profileArea">
             <img src={SearchCharacter.CharacterImage} alt="" className="UserImage" />
 
             <div className="TextArea">
@@ -42,7 +74,7 @@ function CharacterInfo({SearchCharacter, CharacterStats}) {
                     <p>PVP <br/> {SearchCharacter.PvpGradeName}</p>
                 </div>
             </div>
-          </div>
+          </div> */}
 
           <div className="Specifications">
                   <div className="StatArea MiniBox">
